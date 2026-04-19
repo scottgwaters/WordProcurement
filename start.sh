12edf31b017784f884c102183932510d2a5327e0
Scott Waters <scott@installos.com>
2026-04-18 23:08:57 -0400

    Regenerate Prisma client at runtime

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+echo "DATABASE_URL is set: ${DATABASE_URL:+yes}"
+echo "Regenerating Prisma client..."
+npx prisma generate
 echo "Running Prisma db push..."
-npx prisma db push --skip-generate
+npx prisma db push --skip-generate --accept-data-loss
 echo "Starting Next.js..."
 npm start
